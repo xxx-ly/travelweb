5442a3c3b6ca578a55bacab6fb7a0559ca07de5b
Leon <yang.thereal12@gmail.com>
2024-07-09 13:46:56 +0800

    更新2.0

---
+++ js/swiper.js
@@ -0,0 +1,7 @@
+const contentShow = 2 ;
+const moveSpeed = 500;
+
+const carousel = document.querySelector(".carousel");
+const container = document.querySelector(".container");
+const content = document.querySelector(".content");
+console.log(content);
